f9e2961a545a3a23a9012904505eab8d435c79cd
Bishesh22010 <v3n0m.b@gmail.com>
2025-11-13 00:08:03 +0530

    Added New Migrations

---
+++ ErpConsoleApp/Migrations/20251112183632_SecondMigration.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ErpConsoleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parties",
+                keyColumn: "PartyId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parties",
+                keyColumn: "PartyId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parties",
+                keyColumn: "PartyId",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "PurchaseSlips",
+                newName: "SlipDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SlipDate",
+                table: "PurchaseSlips",
+                newName: "Date");
+
+            migrationBuilder.InsertData(
+                table: "Parties",
+                columns: new[] { "PartyId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "XYZ Party" },
+                    { 2, "Main Supplier Inc." },
+                    { 3, "Local Hardware" }
+                });
+        }
+    }
+}
